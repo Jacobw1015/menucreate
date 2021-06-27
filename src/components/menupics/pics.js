cdfd4d6749acb1b2ba7cfac6a510ceec00f19349
Jacob Wenger <81597079+Jacobw1015@users.noreply.github.com>
2021-06-27 09:55:37 -0500

    added pics to project

---
+++ src/components/menupics/pics.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class AddPic extends React.Component{
+   
+    constructor(props){
+        super(props);
+        this.state ={
+          
+        }
+         let pics= [];
+    }
+    selectPic(){
+        
+    }
+
+    render(){
+
+
+
+    }
+}
